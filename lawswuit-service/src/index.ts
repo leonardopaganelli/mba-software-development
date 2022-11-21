143ae946e205427ba8c918256110a14faf175341
Leonardo Paganelli <leonardo.paganelli@nutrien.com>
2022-11-21 17:59:43 -0300

    add swagger

--- lawswuit-service/src/index.ts
+++ lawswuit-service/src/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
-const PORT = 3000;
+import routes from "./routes";
+import swaggerDocs from '@utils/swagger.js';
+
+const port = 3000;
 const app = express();
-app.get("/test", (req, res) => {
-  res.status(200).send("Hello World");
-});
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+
+app.listen(port, () => {
+  console.log(`Server running on port http://127.0.0.1:${port}`);
+  routes(app);
+  swaggerDocs(app, port);
 });
---
+++ lawswuit-service/src/routes.ts
@@ -0,0 +1,22 @@
+import { Express, Request, Response } from "express";
+
+const routes = (app: Express) => {
+  /**
+   * @openapi
+   * /healthcheck:
+   *  get:
+   *     tags:
+   *     - Healthcheck
+   *     description: Responds if the app is up and running
+   *     responses:
+   *       200:
+   *         description: App is up and running
+   */
+  app.get("/healthcheck", (_: Request, res: Response) => res.sendStatus(200));
+
+  app.get("/test", (req, res) => {
+    res.status(200).send("Hello World");
+  });
+}
+
+export default routes;
