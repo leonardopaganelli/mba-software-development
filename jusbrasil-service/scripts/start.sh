3d8522960baefc47260f78331b2824f1eba1a82a
Leonardo Paganelli <leonardo.paganelli@nutrien.com>
2022-11-22 10:23:39 -0300

    adjust docker-compose

---
+++ jusbrasil-service/scripts/start.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# $1 Error message
+fail() {
+  echo "$1"
+  echo "Exiting"
+  exit 1
+}
+
+startService() {
+  echo "Starting jusbrasil service..."
+#   yarn migration:run
+#   yarn schema:register
+  PORT=${PORT:-3001} node dist/index.js
+}
+
+echo "Initializing jusbrasil service environment..."
+
+# Start the service
+
+# Main
+set -e
+CMD=${1:-"start"}
+ENVIRONMENT=${ENVIRONMENT:-"local"}
+
+case "$CMD" in
+start)
+  startService
+  ;;
+*)
+  fail "Invalid command: $CMD"
+esac
---
+++ lawswuit-service/scripts/start.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# $1 Error message
+fail() {
+  echo "$1"
+  echo "Exiting"
+  exit 1
+}
+
+startService() {
+  echo "Starting lawsuit service..."
+#   yarn migration:run
+#   yarn schema:register
+  PORT=${PORT:-3000} node dist/index.js
+}
+
+echo "Initializing lawsuit service environment..."
+
+# Start the service
+
+# Main
+set -e
+CMD=${1:-"start"}
+ENVIRONMENT=${ENVIRONMENT:-"local"}
+
+case "$CMD" in
+start)
+  startService
+  ;;
+*)
+  fail "Invalid command: $CMD"
+esac
