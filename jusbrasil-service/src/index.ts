d04974e7a549206080d770925034cec748ad9162
Leonardo Paganelli <leonardo.paganelli@outlook.com>
2023-01-13 23:55:08 -0300

    add cors config to jusbrasil-service

--- jusbrasil-service/src/index.ts
+++ jusbrasil-service/src/index.ts
@@ -4,13 +4,29 @@ import swaggerDocs from "@utils/swagger";
 import config from "@utils/config";
 import kafka  from "@clients/kafka";
 import { connect as databaseConnect } from "@clients/database";
+import cors from "cors";
 
 console.log("Initializing with config", config);
+
+const options: cors.CorsOptions = {
+  allowedHeaders: [
+    "Origin",
+    "X-Requested-With",
+    "Content-Type",
+    "Accept",
+    "X-Access-Token",
+  ],
+  credentials: true,
+  methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+  origin: ["http://localhost:8081", "http://localhost:8080"],
+  preflightContinue: false,
+};
 
 const port = 3001;
 const app = express();
 
 app.use(express.json());
+app.use(cors(options));
 
 async function run() {
   await kafka(app);
